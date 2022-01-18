478316fc1c818806e77a8358ef60285f7b5f7767
Myke Pedro Paduelo <pedropaduelo@gmail.com>
2022-01-18 10:22:41 -0300

    vai

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    // baseURL: 'https://notcodeapi.herokuapp.com/',
-    baseURL: 'http://localhost:3010/',
+    baseURL: 'https://notcodeapi.herokuapp.com/',
+    // baseURL: 'http://localhost:3010/',
 })
 
 export default api;
